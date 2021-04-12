52749b9cc0ec8a76b84963b4be282975dcbfc613
James <james.rosstwo@gmail.com>
2021-04-11 22:22:56 -0700

    add astar pathfinding

--- agent/environment/board.py
+++ agent/environment/board.py
@@ -1,7 +1,10 @@
+# Partially taken from https://medium.com/@nicholas.w.swift/easy-a-star-pathfinding-7e6689c7f7b2
+
 from collections import namedtuple
 
 from agent.actions.action import BattleSnakeAction
 from agent.environment.cell import BattleSnakeCellType, BattleSnakeCell, cell_symbols
+
 
 class BoardCoord:
     def __init__(self, x, y):
@@ -13,6 +16,21 @@ class BoardCoord:
 
     def __sub__(self, other):
         return BoardCoord(self.x - other.x, self.y - other.y)
+
+
+class SearchNode:
+    """A node class for A* Pathfinding"""
+
+    def __init__(self, parent=None, position: BoardCoord = None):
+        self.parent = parent
+        self.position = position
+
+        self.g = 0
+        self.h = 0
+        self.f = 0
+
+    def __eq__(self, other):
+        return self.position == other.position
 
 
 class BattleSnakeBoard:
@@ -32,6 +50,9 @@ class BattleSnakeBoard:
 
     def get_cell(self, x, y) -> BattleSnakeCell:
         return self.cells[self.height - y - 1][x]
+
+    def get_cell_from_coord(self, coord: BoardCoord):
+        return self.get_cell(coord.x, coord.y)
 
     def _set_cell(self, x, y, t: BattleSnakeCellType):
         self.cells[self.height - y - 1][x] = BattleSnakeCell(x, y, t)
@@ -53,44 +74,93 @@ class BattleSnakeBoard:
     # BFS Pathfinding on grid from a to b
     # Template: https://www.geeksforgeeks.org/breadth-first-search-or-bfs-for-a-graph/
     @staticmethod
-    def next_direction_to(a: BoardCoord, b: BoardCoord):
+    def next_direction_to(self, a: BoardCoord, b: BoardCoord):
 
-        # neighbour_offsets = [BoardCoord(-1, 0), BoardCoord(1, 0), BoardCoord(0, -1), BoardCoord(0, 1)]
-        #
-        # # Function to print a BFS of graph
-        # def bfs(self, s):
-        #     # Create a queue for BFS
-        #     queue = [s]
-        #
-        #     while queue:
-        #
-        #         # Dequeue a vertex from
-        #         # queue and print it
-        #         s = queue.pop(0)
-        #         print(s, end=" ")
-        #
-        #         # Get all adjacent vertices of the
-        #         # dequeued vertex s. If a adjacent
-        #         # has not been visited, then mark it
-        #         # visited and enqueue it
-        #         for offset in neighbour_offsets:
-        #             new_pos = s + offset
-        #             new_cell = self.get_cell(new_pos)
-        #             if not new_cell.seen:
-        #                 queue.append(new_cell)
-        #                 new_cell.seen = True
-        #
-        # heuristic = BattleSnakeBoard.dist
+        neighbour_offsets = [BoardCoord(-1, 0), BoardCoord(1, 0), BoardCoord(0, -1), BoardCoord(0, 1)]
 
-        d = b - a
-        if d.x > 0:
-            return BattleSnakeAction.RIGHT
-        elif d.x < 0:
-            return BattleSnakeAction.LEFT
+        def astar(start, end):
+            """Returns a list of tuples as a path from the given start to the given end in the given maze"""
 
-        if d.y > 0:
-            return BattleSnakeAction.UP
-        return BattleSnakeAction.DOWN
+            # Create start and end node
+            start_node = SearchNode(None, start)
+            start_node.g = start_node.h = start_node.f = 0
+            end_node = SearchNode(None, end)
+            end_node.g = end_node.h = end_node.f = 0
+
+            # Initialize both open and closed list
+            open_list = []
+            closed_list = []
+
+            # Add the start node
+            open_list.append(start_node)
+
+            # Loop until you find the end
+            while len(open_list) > 0:
+
+                # Get the current node
+                current_node = open_list[0]
+                current_index = 0
+                for index, item in enumerate(open_list):
+                    if item.f < current_node.f:
+                        current_node = item
+                        current_index = index
+
+                # Pop current off open list, add to closed list
+                open_list.pop(current_index)
+                closed_list.append(current_node)
+
+                # Found the goal
+                if current_node == end_node:
+                    path = []
+                    current = current_node
+                    while current is not None:
+                        path.append(current.position)
+                        current = current.parent
+                    return path[::-1]  # Return reversed path
+
+                # Generate children
+                children = []
+                for offset in neighbour_offsets:  # Adjacent squares
+
+                    # Get node position
+                    node_position = current_node.position + offset
+
+                    # Make sure within range
+                    if not self.is_valid(offset):
+                        continue
+
+                    # Make sure walkable terrain
+                    if self.get_cell(node_position).type == BattleSnakeCellType.DANGER:
+                        continue
+
+                    # Create new node
+                    new_node = SearchNode(current_node, node_position)
+
+                    # Append
+                    children.append(new_node)
+
+                # Loop through children
+                for child in children:
+
+                    # Child is on the closed list
+                    for closed_child in closed_list:
+                        if child == closed_child:
+                            continue
+
+                    # Create the f, g, and h values
+                    child.g = current_node.g + 1
+                    child.g = self.dist(child.position, end_node.position)
+                    child.f = child.g + child.h
+
+                    # Child is already in the open list
+                    for open_node in open_list:
+                        if child == open_node and child.g > open_node.g:
+                            continue
+
+                    # Add the child to the open list
+                    open_list.append(child)
+
+        return astar(a, b)
 
     # Manhattan distance because we're locked to a grid
     @staticmethod
