b5e8c8d5e2b2e50b6cd8dd87a644b3518cf2c221
Matthew <matthewegan00@gmail.com>
2021-04-16 00:26:25 -0400

    fixed colour code format

--- server.py
+++ server.py
@@ -18,7 +18,7 @@ class Battlesnake(object):
 
     def index(self):
         rbc_blue = "#0059b3"
-        rbc_gold = "ffdd01"
+        rbc_gold = "#ffdd01"
         color = random.choice([rbc_gold, rbc_blue])
         # This function is called when you register your Battlesnake on play.battlesnake.com
         # It controls your Battlesnake appearance and author permissions.
